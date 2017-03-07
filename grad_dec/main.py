97792be22f587345a650beb7cee964f9f0dc2614
Bradley Whitlock <bradley.whitlock@rubikloud.com>
2017-03-06 21:42:20 -0500

    change data format, implement cost function to stop when best hypothesis

--- grad_dec/main.py
+++ grad_dec/main.py
@@ -7,33 +7,41 @@ from helper import (max_multi_dim_array, min_multi_dim_array, avg_array, avg_mul
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-def line_error(theta, points):
+def line_error(theta, x_data, y_data):
     totalError = 0
-    for i in range(0, len(points)):
-        x = points[i][0]
-        y = points[i][1]
+    for i in range(0, len(x_data)):
+        x = x_data[i][1]
+        y = y_data[i]
         totalError += (y - hyp(theta, x)) ** 2
-    return totalError / float(len(points))
+    return totalError / float(len(x_data))
 
 def hyp(theta, x):
     return theta[0] * x + theta[1]
 
-def step_gradient(theta, points, learningRate):  
+def step_gradient(theta, x_data, y_data, learningRate): 
     t_gradient = [0] * len(theta)
-    N = float(len(points))
-    for i in range(0, len(points)):
-        x = points[i][0]
-        y = points[i][1]
+    N = float(len(x_data))
+    for i in range(0, len(x_data)):
+        x = x_data[i][1]
+        y = y_data[i]
         t_gradient[0] += x * (hyp(theta, x) - y)
         t_gradient[1] += (hyp(theta, x) - y)
     theta[1] = theta[1] - (learningRate * t_gradient[0])/N
     theta[0] = theta[0] - (learningRate * t_gradient[1])/N
     return theta
 
-def gradient_descent_runner(points, theta, learning_rate, num_iterations):
+def gradient_descent_runner(x, y, theta, learning_rate, num_iterations):
+    last_error = line_error(theta, x, y)
+    count = 0
     for i in range(num_iterations):
-        temp_theta = step_gradient(theta, array(points), learning_rate)
-    return temp_theta
+        theta = step_gradient(theta, x, y, learning_rate)
+        curr_error = line_error(theta, x, y)
+        if last_error < curr_error:
+            break
+        last_error = curr_error
+        count += 1
+    print "Number of itterations: ", count
+    return theta
 
 def float_wrapper(reader):
     for v in reader:
@@ -41,48 +49,50 @@ def float_wrapper(reader):
         yield map(float, v)
 
 # Improvment: use std. dev. rather than range
-def feature_scale(points, index):
-    range_data = max_multi_dim_array(points, index) - min_multi_dim_array(points, index)
-    average_data = avg_multi_dim_array(points, index)
-    for i in range(0, len(points)):
-        points[i][index] = float(points[i][index] - average_data) / range_data
-    return points
+def feature_scale(data, index):
+    x_vals = map(lambda x: x[index], data)
+    std_dev = np.std(x_vals)
+    #range_data = max_multi_dim_array(points, index) - min_multi_dim_array(points, index)
+    #print range_data
+    average_data = avg_multi_dim_array(data, index)
+    for i in range(0, len(data)):
+        data[i][index] = float(data[i][index] - average_data) / std_dev
+    return data
 
 def populate(file_name):
-    #X = []
-    #Y = []
+    X = []
+    Y = []
     points = []
     with open(dir_path + '/' + file_name, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter= ',')
         reader = float_wrapper(reader)
         for row in reader:
-            #X.append([1])
-            #X[len(X) - 1] += row[0:len(row) - 1]
-            #Y.append(row[len(row) - 1])
+            X.append([1])
+            X[len(X) - 1] += row[0:len(row) - 1]
+            Y.append(row[len(row) - 1])
             points.append(row)
-    return points
-
+    return (X, Y)
 
 def run():
-    points = populate('housing_data.csv')
+    X, Y = populate('housing_data.csv')
     # initial guess for the coefficients
-    theta = [0] * len(points[0])
-    learning_rate = 0.0003
-    num_iterations = 5000
-    #print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points))
-    print "Running..."
-    theta = gradient_descent_runner(points, theta, learning_rate, num_iterations)
-    #print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points))
+    theta = [0] * len(X[0])
+    learning_rate = 0.0005
+    max_num_iterations = 2000
 
-    X = []
-    Y = []
-    for row in points:
-        X.append(row[0])
-        Y.append(row[1])
-    plt.plot(X, Y, 'ro')
-    x = np.array(range(-2, 2))
-    formula = str(str(theta[1]) + '+' + str(theta[0]) + '*x')   
-    print formula
+    print X
+    #X = feature_scale(X, 1)
+    print X
+
+    print "Running..."
+
+    theta = gradient_descent_runner(X, Y, theta, learning_rate, max_num_iterations)
+    x_plot = map(lambda x: x[1], X)
+    print x_plot
+    plt.plot(x_plot, Y, 'ro')
+    x = np.array(range(0, int(max(x_plot) * 1.15)))
+    formula = str(str(theta[1]) + '+' + str(theta[0]) + '*x')
+    print formula   
     y = eval(formula)
     plt.plot(x, y)
     plt.show()
@@ -90,3 +100,12 @@ def run():
 
 if __name__ == '__main__':
     run()
+
+# I have noticed that as the "X" data gets smaller need to do 1
+#   of 2 things to improve performance
+# 1. increase learning learning
+# 2. increase the number of itterations
+#   - Also noticed that small change in learning rate dramatically
+#       reduces the number of necessary itterations
+# This is because with smaller changes(errors) the change each 
+#   itteration is smaller
