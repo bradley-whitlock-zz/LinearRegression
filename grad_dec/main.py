1dde8bc8c28ca431acdb1a853358bfa166bca29a
Bradley Whitlock <bradley.whitlock@rubikloud.com>
2017-03-12 12:18:56 -0400

    Create next steps and clean code/dependencies

--- grad_dec/main.py
+++ grad_dec/main.py
@@ -18,32 +18,30 @@ def line_error(theta, x_data, y_data):
 # Accepts x which is array of training data so can customize the form
 def hyp(theta, x):
     # Form y = mx + b
-    return theta[0] * x[1] + theta[1]
+    return theta[0] * x[1] +  theta[1]
 
-def step_gradient(theta, x_data, y_data, learningRate, num_dimensions): 
+def step_gradient(theta, x_data, y_data, learningRate, num_factors): 
     t_gradient = 0
-    temp_theta = [0] * num_dimensions
+    temp_theta = [0] * num_factors
     N = float(len(x_data))
-    for j in range(0, num_dimensions):
+    for j in range(0, num_factors):
         for i in range(0, len(x_data)):
             t_gradient += x_data[i][j] * (hyp(theta, x_data[i]) - y_data[i])
         temp_theta[j] = theta[j] - (learningRate * t_gradient)/N
     return temp_theta
 
-def gradient_descent_runner(x, y, theta, learning_rate, max_num_iterations, num_dimensions):
+def gradient_descent_runner(x, y, theta, learning_rate, max_num_iterations, num_factors):
     last_error = line_error(theta, x, y)
-    last_t = [0] * num_dimensions
     count = 0
     for i in range(max_num_iterations):
-        theta = step_gradient(theta, x, y, learning_rate, num_dimensions)
+        theta = step_gradient(theta, x, y, learning_rate, num_factors)
         curr_error = line_error(theta, x, y)
         if last_error < curr_error:
             break
-        last_t = theta
         last_error = curr_error
         count += 1
     print "Number of itterations: ", count
-    return last_t
+    return theta
 
 def float_wrapper(reader):
     for v in reader:
@@ -54,8 +52,6 @@ def float_wrapper(reader):
 def feature_scale(data, index):
     x_vals = map(lambda x: x[index], data)
     std_dev = np.std(x_vals)
-    #range_data = max_multi_dim_array(points, index) - min_multi_dim_array(points, index)
-    #print range_data
     average_data = avg_multi_dim_array(data, index)
     for i in range(0, len(data)):
         data[i][index] = float(data[i][index] - average_data) / std_dev
@@ -64,35 +60,37 @@ def feature_scale(data, index):
 def populate(file_name):
     X = []
     Y = []
-    points = []
     with open(dir_path + '/' + file_name, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter= ',')
         reader = float_wrapper(reader)
         for row in reader:
+            # This is for completeness, first factor is always 1
             X.append([1])
+            # This is where the data is inputted from CSV
             X[len(X) - 1] += row[0:len(row) - 1]
             Y.append(row[len(row) - 1])
-            points.append(row)
     return (X, Y)
 
 def run():
     X, Y = populate('housing_data.csv')
 
-    num_dimensions = 2
-    theta = [0] * num_dimensions
+    # num_factors will always be 1 greater than the actual number of factors becasue of constant coeff
+    num_factors = 2
+    theta = [0] * (num_factors)
 
-    learning_rate = 0.0003
-    max_num_iterations = 3000
+    # Coeff's user sets
+    learning_rate = 0.001
+    max_num_iterations = 100000
 
     print X
     #X = feature_scale(X, 1)
-    print X
+    #print X
 
     print "Running..."
-
-    theta = gradient_descent_runner(X, Y, theta, learning_rate, max_num_iterations, num_dimensions)
+    theta = gradient_descent_runner(X, Y, theta, learning_rate, max_num_iterations, num_factors)
+    
     x_plot = map(lambda x: x[1], X)
-    print 'Max xplot: ', max(x_plot)
+    #print 'Max xplot: ', max(x_plot)
     plt.plot(x_plot, Y, 'ro')
     x = np.array(range(0, int(max(x_plot) * 1.15)))
     formula = str(str(theta[1]) + '+' + str(theta[0]) + '*x')
@@ -101,7 +99,6 @@ def run():
     #plt.axis([-2, 2, 0, 250])
     plt.plot(x, y)
     plt.show()
-
 
 if __name__ == '__main__':
     run()
@@ -114,3 +111,18 @@ if __name__ == '__main__':
 #       reduces the number of necessary itterations
 # This is because with smaller changes(errors) the change each 
 #   itteration is smaller
+
+# Works with this data in csv
+
+# Next step is to add another factor
+
+
+'''
+-2,20
+-1,15
+0,10
+1,5
+2,0
+3,-5
+4,-10
+'''
